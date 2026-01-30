70a0e0a7ee4d24c72dfec42cc4c9c814baad0831
Brnwj173 <155011416+Brnwj173@users.noreply.github.com>
2026-01-30 03:46:27 +0100

    Update skripta.js

--- GameZone - PC Igraonica/scripts/skripta.js
+++ GameZone - PC Igraonica/scripts/skripta.js
@@ -1,16 +1,17 @@
 /*jshint esversion: 6 */
 
-const contactForm = document.getElementById('contactForm');
+var contactForm = document.getElementById('contactForm');
 
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
     
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const message = document.getElementById('message').value;
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
     
     alert(`Hvala ${name}! Vaša poruka je uspešno poslata.\n\nKontaktiraćemo vas uskoro na: ${email}`);
     
     contactForm.reset();
 });
+
